601b9b69cfccf69008152a3160b01acde3f08151
Moktadir Rahman <autocircled@gmail.com>
2018-09-04 11:21:38 +0600

    Completed calculator app using data binding

--- app/src/main/java/org/autocircle/autocircle/DisplayMessage.java
+++ app/src/main/java/org/autocircle/autocircle/DisplayMessage.java
@@ -1,116 +1,144 @@
 package org.autocircle.autocircle;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import org.autocircle.autocircle.databinding.ActivityDisplayMessageBinding;
+
+import java.text.DecimalFormat;
 
 public class DisplayMessage extends AppCompatActivity implements View.OnClickListener{
-    EditText userInput;
-    TextView result;
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnMul, btnDiv, btnSub, btnAdd,
-    btnDot, btnResult;
-
+//    EditText userInput;
+//    TextView result;
+//    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnMul, btnDiv, btnSub, btnAdd,
+//    btnDot, btnResult;
+ActivityDisplayMessageBinding binding;
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
+    private char CURRENT_ACTION;
+    private double valueOne = Double.NaN;
+    private double valueTwo;
+    private DecimalFormat decimalFormat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        decimalFormat = new DecimalFormat("#.##########");
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_display_message);
 
-        initVars();
+//        initVars();
+        binding.bZero.setOnClickListener(this);
+        binding.bOne.setOnClickListener(this);
+        binding.bTwo.setOnClickListener(this);
+        binding.bThree.setOnClickListener(this);
+        binding.bFour.setOnClickListener(this);
+        binding.bFive.setOnClickListener(this);
+        binding.bSix.setOnClickListener(this);
+        binding.bSeven.setOnClickListener(this);
+
+        binding.bEight.setOnClickListener(this);
+        binding.bNine.setOnClickListener(this);
+        binding.bDot.setOnClickListener(this);
+        binding.bMultiply.setOnClickListener(this);
+        binding.bDivision.setOnClickListener(this);
+        binding.bSubstruction.setOnClickListener(this);
+        binding.bAddition.setOnClickListener(this);
+        binding.bResult.setOnClickListener(this);
+
+
     }
+    private void computeCalculation() {
+        if(!Double.isNaN(valueOne)) {
+            valueTwo = Double.parseDouble(binding.etUserInput.getText().toString());
+            binding.etUserInput.setText(null);
 
-    private void initVars() {
-        btn0 = findViewById(R.id.bZero);
-        btn1 = findViewById(R.id.bOne);
-        btn2 = findViewById(R.id.bTwo);
-        btn3 = findViewById(R.id.bThree);
-        btn4 = findViewById(R.id.bFour);
-        btn5 = findViewById(R.id.bFive);
-        btn6 = findViewById(R.id.bSix);
-        btn7 = findViewById(R.id.bSeven);
-        btn8 = findViewById(R.id.bEight);
-        btn9 = findViewById(R.id.bNine);
-        btnDot = findViewById(R.id.bDot);
-        btnMul = findViewById(R.id.bMultiply);
-        btnDiv = findViewById(R.id.bDivision);
-        btnSub = findViewById(R.id.bSubstruction);
-        btnAdd = findViewById(R.id.bAddition);
-        btnResult = findViewById(R.id.bResult);
-        result = findViewById(R.id.tvDisplayResult);
-        userInput = findViewById(R.id.etUserInput);
-
-        btn0.setOnClickListener(this);
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
-        btn7.setOnClickListener(this);
-        btn8.setOnClickListener(this);
-        btn9.setOnClickListener(this);
-        btnDot.setOnClickListener(this);
-        btnMul.setOnClickListener(this);
-        btnDiv.setOnClickListener(this);
-        btnSub.setOnClickListener(this);
-        btnAdd.setOnClickListener(this);
-        btnResult.setOnClickListener(this);
+            if(CURRENT_ACTION == ADDITION)
+                valueOne = this.valueOne + valueTwo;
+            else if(CURRENT_ACTION == SUBTRACTION)
+                valueOne = this.valueOne - valueTwo;
+            else if(CURRENT_ACTION == MULTIPLICATION)
+                valueOne = this.valueOne * valueTwo;
+            else if(CURRENT_ACTION == DIVISION)
+                valueOne = this.valueOne / valueTwo;
+        }
+        else {
+            try {
+                valueOne = Double.parseDouble(binding.etUserInput.getText().toString());
+            }
+            catch (Exception e){}
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.bZero:
-                userInput.append(btn0.getText().toString());
+                binding.etUserInput.append(binding.bZero.getText());
                 break;
             case R.id.bOne:
-                userInput.append(btn1.getText().toString());
+                binding.etUserInput.append(binding.bOne.getText());
                 break;
             case R.id.bTwo:
-                userInput.append(btn2.getText().toString());
+                binding.etUserInput.append(binding.bTwo.getText());
                 break;
             case R.id.bThree:
-                userInput.append(btn3.getText().toString());
+                binding.etUserInput.append(binding.bThree.getText());
                 break;
             case R.id.bFour:
-                userInput.append(btn4.getText().toString());
+                binding.etUserInput.append(binding.bFour.getText());
                 break;
             case R.id.bFive:
-                userInput.append(btn5.getText().toString());
+                binding.etUserInput.append(binding.bFive.getText());
                 break;
             case R.id.bSix:
-                userInput.append(btn6.getText().toString());
+                binding.etUserInput.append(binding.bSix.getText());
                 break;
             case R.id.bSeven:
-                userInput.append(btn7.getText().toString());
+                binding.etUserInput.append(binding.bSeven.getText());
                 break;
             case R.id.bEight:
-                userInput.append(btn8.getText().toString());
+                binding.etUserInput.append(binding.bEight.getText());
                 break;
             case R.id.bNine:
-                userInput.append(btn9.getText().toString());
+                binding.etUserInput.append(binding.bNine.getText());
                 break;
             case R.id.bDot:
-                userInput.append(btnDot.getText().toString());
+                binding.etUserInput.append(binding.bDot.getText());
                 break;
             case R.id.bMultiply:
-
+                computeCalculation();
+                CURRENT_ACTION = MULTIPLICATION;
+                binding.tvDisplayResult.setText(decimalFormat.format(valueOne) + "*");
+                binding.etUserInput.setText(null);
                 break;
             case R.id.bDivision:
-
+                computeCalculation();
+                CURRENT_ACTION = DIVISION;
+                binding.tvDisplayResult.setText(decimalFormat.format(valueOne) + "/");
+                binding.etUserInput.setText(null);
                 break;
             case R.id.bSubstruction:
-
+                computeCalculation();
+                CURRENT_ACTION = SUBTRACTION;
+                binding.tvDisplayResult.setText(decimalFormat.format(valueOne) + "-");
+                binding.etUserInput.setText(null);
                 break;
             case R.id.bAddition:
-
+                computeCalculation();
+                CURRENT_ACTION = ADDITION;
+                binding.tvDisplayResult.setText(decimalFormat.format(valueOne) + "+");
+                binding.etUserInput.setText(null);
                 break;
             case R.id.bResult:
-
+                computeCalculation();
+                binding.tvDisplayResult.setText(binding.tvDisplayResult.getText().toString() +
+                        decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+                valueOne = Double.NaN;
+                CURRENT_ACTION = '0';
                 break;
-
         }
     }
 }
