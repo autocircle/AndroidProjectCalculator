b0864b7a217b50c64547999cc889a86d8d139adc
Moktadir Rahman <autocircled@gmail.com>
2018-09-04 10:08:34 +0600

    Saving files before refreshing line endings

--- app/src/main/java/org/autocircle/autocircle/DisplayMessage.java
+++ app/src/main/java/org/autocircle/autocircle/DisplayMessage.java
@@ -1,8 +1,7 @@
 package org.autocircle.autocircle;
 
-import android.support.constraint.solver.ArrayLinkedVariables;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
